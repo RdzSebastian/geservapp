21fd65bd806937a3ad53e47e6e71f198f8f4b1d0
RdzSebastian <rdzsebastian@gmail.com>
2022-02-26 16:06:02 -0300

    Start

---
+++ src/main/java/com/estonianport/geservapp/GeservappApplication.java
@@ -0,0 +1,13 @@
+package com.estonianport.geservapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeservappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeservappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estonianport/geservapp/GeservappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estonianport.geservapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeservappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
