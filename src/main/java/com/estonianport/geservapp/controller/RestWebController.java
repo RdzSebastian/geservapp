644a965fbdb7c17921ffd59346a637b9b501dc26
RdzSebastian <rdzsebastian@gmail.com>
2022-03-01 18:38:18 -0300

    Adds Spring Security

--- src/main/java/com/estonianport/geservapp/controller/RestWebController.java
+++ src/main/java/com/estonianport/geservapp/controller/RestWebController.java
@@ -1,13 +1,11 @@
 package com.estonianport.geservapp.controller;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
