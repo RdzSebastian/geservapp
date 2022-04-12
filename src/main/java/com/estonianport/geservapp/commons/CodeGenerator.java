34512cdcd536a4b971006efc5ac992ff88ad3a4e
RdzSebastian <rdzsebastian@gmail.com>
2022-04-12 13:46:11 -0300

    Add base26 code generator for event search

---
+++ src/main/java/com/estonianport/geservapp/commons/CodeGenerator.java
@@ -0,0 +1,19 @@
+package com.estonianport.geservapp.commons;
+
+import java.util.Random;
+
+public class CodeGenerator {
+   
+    public static String GetBase26Only4Letters() {
+    	Random random = new Random();
+    	char[] base26chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    	
+        var sb = new StringBuilder(4);
+
+        for (int i=0; i<4; i++) {
+            sb.append(base26chars[random.nextInt(24)]);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/estonianport/geservapp/container/CodigoContainer.java
@@ -0,0 +1,14 @@
+package com.estonianport.geservapp.container;
+
+public class CodigoContainer {
+
+	private String codigo;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+}
