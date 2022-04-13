68fbc9b5ee5c52ac3c437a7052cc37a731eb48dc
RdzSebastian <rdzsebastian@gmail.com>
2022-04-13 16:28:31 -0300

    The administration panel can only be seen by the admin role

--- src/main/java/com/estonianport/geservapp/controller/EventoController.java
+++ src/main/java/com/estonianport/geservapp/controller/EventoController.java
@@ -3,6 +3,7 @@ package com.estonianport.geservapp.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import  com.estonianport.geservapp.commons.GeneralPath;
 import com.estonianport.geservapp.model.Salon;
 import com.estonianport.geservapp.service.EventoService;
+import com.estonianport.geservapp.service.RolService;
 import com.estonianport.geservapp.service.SalonService;
+import com.estonianport.geservapp.service.UsuarioService;
 
 @Controller
 public class EventoController {
@@ -22,11 +25,18 @@ public class EventoController {
 
 	@Autowired
 	private SalonService salonService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private RolService rolService;
 
 	@RequestMapping("/abmEvento/{id}")
-	public String abm(@PathVariable("id") Long id, Model model, HttpSession session) {
+	public String abm(@PathVariable("id") Long id, Model model, HttpSession session, Authentication authentication) {
 		Salon salon = salonService.get(id);
 		session.setAttribute("salon", salon);
+		model.addAttribute("admin", usuarioService.findUserByUsername(authentication.getName()).getRol() == rolService.get((long)1));
 		return GeneralPath.EVENTO + GeneralPath.PATH_SEPARATOR + GeneralPath.ABM_EVENTO;
 	}
 
