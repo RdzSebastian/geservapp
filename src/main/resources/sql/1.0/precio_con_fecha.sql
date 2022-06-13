1675984b84d9a6aec6de2d77c3bea1ed1e3a4762
RdzSebastian <rdzsebastian@gmail.com>
2022-06-13 13:00:00 -0300

    Add default link between all subtipoEvento and PrecioConFecha

---
+++ src/main/resources/sql/1.0/precio_con_fecha.sql
@@ -0,0 +1,35 @@
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2022-01-01 00:00:00.000','2022-04-30 23:59:00.000',15);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2022-05-01 00:00:00.000','2022-08-31 23:59:00.000',30);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2022-09-01 00:00:00.000','2022-10-31 23:59:00.000',45);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2022-11-01 00:00:00.000','2022-12-31 23:59:00.000',60);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2023-01-01 00:00:00.000','2023-04-30 23:59:00.000',75);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2023-05-01 00:00:00.000','2023-08-31 23:59:00.000',90);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2023-09-01 00:00:00.000','2023-10-31 23:59:00.000',105);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2023-11-01 00:00:00.000','2023-12-31 23:59:00.000',120);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2024-01-01 00:00:00.000','2024-04-30 23:59:00.000',135);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2024-05-01 00:00:00.000','2024-08-31 23:59:00.000',150);
+
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2024-09-01 00:00:00.000','2024-10-31 23:59:00.000',175);
+	
+INSERT INTO precio_con_fecha (desde,hasta,precio)
+	VALUES ('2024-11-01 00:00:00.000','2024-12-31 23:59:00.000',190);
---
+++ src/main/resources/sql/1.0/sub_tipo_evento_precio_con_fecha.sql
@@ -0,0 +1,249 @@
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (1,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (2,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (3,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (4,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (5,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (6,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (7,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (8,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (9,12);
+
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,1);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,2);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,3);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,4);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,5);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,6);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,7);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,8);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,9);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,10);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,11);
+INSERT INTO sub_tipo_evento_precio_con_fecha (sub_tipo_evento_id,precio_con_fecha_id)
+	VALUES (10,12);
